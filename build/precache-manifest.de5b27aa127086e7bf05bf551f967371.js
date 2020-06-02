9d964c9941eee21e3ca24d720e061b9925207b75
Mubashir <mubashir.akhtar@laksol.com>
2020-06-02 21:24:12 +0500

    upload build folder

---
+++ build/precache-manifest.de5b27aa127086e7bf05bf551f967371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b37a4df8e9649e0ec8f1f712bde816c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81eecb7cfd52542defcb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c9819b1b0cf8074495b",
+    "url": "/static/js/2.3a3e0656.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a3e0656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81eecb7cfd52542defcb",
+    "url": "/static/js/main.3518cc1e.chunk.js"
+  },
+  {
+    "revision": "f3a87a1cf86047070d79",
+    "url": "/static/js/runtime-main.315bf881.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
